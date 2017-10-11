9d4c2093fbe74d6406d8f5d5b698e1ed46e6ff48
MariaPaulaP <maria.pinzon-c@mail.escuelaing.edu.co>
2017-10-11 13:33:53 -0500

    initial commit

---
+++ src/main/java/edu/eci/arsw/RalphWindows/RalphWindowsApplication.java
@@ -0,0 +1,12 @@
+package edu.eci.arsw.RalphWindows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RalphWindowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RalphWindowsApplication.class, args);
+	}
+}
